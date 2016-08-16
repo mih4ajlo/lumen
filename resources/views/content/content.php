df2e0d26b43890dd60cbd50266c12780994d1be0
mihajlo <mihajlo88@gmail.com>
2016-08-16 18:11:02 +0200

    dodatni atributi u tinymce

--- resources/views/content/content.php
+++ resources/views/content/content.php
@@ -11,6 +11,7 @@
 	<script>tinymce.init({ 
 		selector:'textarea',
 		 visual: false,
+		 valid_elements : '*[*]',
 		plugins : [
 		  'advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker',
 		  'searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking',
