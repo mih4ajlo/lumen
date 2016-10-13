0e5f9bd83c1fb66728429e252b10aad6244e3957
mihajlo <mihajlo88@gmail.com>
2016-10-13 14:45:20 +0200

    google analytics

--- resources/views/index.php
+++ resources/views/index.php
@@ -212,7 +212,7 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-85089175-1', 'auto');
+  ga('create', 'UA-76516915-1', 'auto');
   ga('send', 'pageview');
 
 </script>
